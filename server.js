32b441b1b8d831ad7d2962335fe0fff26f9b5e49
jshan98 <jshanahan@ggc.edu>
2026-04-23 09:34:34 -0400

    Imported required references for establishing Express.js server

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import expenseRoutes from './routes/expenseRoutes';
