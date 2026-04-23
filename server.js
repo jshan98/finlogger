7d3ca6507a7696036dad8ec2e46b544135bc4f18
jshan98 <jshanahan@ggc.edu>
2026-04-23 09:45:28 -0400

    Added logic for middleware, connecting to MongoDB, and listening for requests

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import expenseRoutes from './routes/expenseRoutes';
+
+const app = express();
+app.use(cors()); // Allows for cross-origin requests
+app.use(bodyParser.json) // parses the JSON request body
+
+mongoose.connect('mongodb://localhost:27017/finlogger/?directConnection=true')
+.then(() => console.log('Connected to MongoDB Server'))
+.catch(error => console.error(`Error connecting to MongoDB Server: ${error}`));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+});
