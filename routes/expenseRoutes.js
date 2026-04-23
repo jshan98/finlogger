b5a1637c54077b0083d0cb6c827748da408762f6
jshan98 <jshanahan@ggc.edu>
2026-04-23 10:32:52 -0400

    Created router in expenseRoutes.js and renamed controller directory to controllers

---
+++ routes/expenseRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as expenseController from '../controllers/expenseController';
+import * as expenseCategoryController from '../controllers/expenseCategoryController';
+
+// Creates router instance
+const router = express.Router();
+
+export default router;
