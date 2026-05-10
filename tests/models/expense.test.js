aaea6fde5da7ad22ef63a6ed42eb7205d436c0b4
jshan98 <jshanahan@ggc.edu>
2026-05-10 19:59:44 -0400

    Imported necessary modules and packages

---
+++ tests/models/expense.test.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import { expect } from 'chai';
+import Expense from '../../models/expense.js';
+import ExpenseCategory from '../../models/expenseCategory.js';
+import '../test_helper.js';
