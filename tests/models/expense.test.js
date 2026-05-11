d0d2a8a94fb4021342b0abd902b186bae592f061
jshan98 <jshanahan@ggc.edu>
2026-05-10 21:01:43 -0400

    Created test case for the update expense functionality

--- tests/models/expense.test.js
+++ tests/models/expense.test.js
@@ -24,8 +24,31 @@ describe('Expense Modal', () => {
 
         const savedExpense = await expense.save();
 
-        expect(savedExpense._id).to.exist; // Checks that the expense has an ID, indicating it was saved successfully
-        expect(savedExpense.user_id).to.equal('USER_2'); // Check that the user ID matches USER_2
+        expect(savedExpense._id).to.exist; // Checks that the expense has an ID, indicating it was saved successfully.
+        expect(savedExpense.user_id).to.equal('USER_2'); // Checks that the user ID matches USER_2.
+
+    });
+
+    // Test case: Checks if an expense can be updated successfully
+    it('Should update an expense successfully', async () => {
+        const expense = new Expense({
+            user_id: 'USER_2',
+            description: 'Dinner',
+            amount: 20,
+            date: new Date(),
+            category_id: category._id
+        });
+
+        const savedExpense = await expense.save();
+
+        savedExpense.description = 'Family Dinner';
+        savedExpense.amount = 25;
+
+        const updatedExpense = await savedExpense.save();
+
+        expect(updatedExpense.description).to.equal('Family Dinner'); // Checks that decription has been updated.
+        expect(updatedExpense.amount).to.equal(25); // Checks that the amount has been updated.
+        expect(updatedExpense._id).to.equal(savedExpense._id); // Checks that the correct expense has been updated.
 
     });
 });
