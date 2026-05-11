c1a2cc8a2b3dde03aaa4b5bf1ae7d5b4375ce4e5
jshan98 <jshanahan@ggc.edu>
2026-05-10 20:03:06 -0400

    Created test suite for Expense Modal

--- tests/models/expense.test.js
+++ tests/models/expense.test.js
@@ -3,3 +3,12 @@ import { expect } from 'chai';
 import Expense from '../../models/expense.js';
 import ExpenseCategory from '../../models/expenseCategory.js';
 import '../test_helper.js';
+
+describe('Expense Modal', () => {
+    let category;
+
+    // Creates a new category in the database before each test.
+    beforeEach(async () => {
+        category = await ExpenseCategory.create({_id:"FOOD", name:'Food'});
+    });
+});
