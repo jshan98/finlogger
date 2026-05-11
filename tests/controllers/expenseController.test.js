b0d66c2fd397563635f360b8ad99b75392107b19
jshan98 <jshanahan@ggc.edu>
2026-05-11 07:44:29 -0400

    Created routes and test suite for express controller testing

--- tests/controllers/expenseController.test.js
+++ tests/controllers/expenseController.test.js
@@ -1,7 +1,23 @@
 import request from 'supertest';
 import express from 'express';
 import { expect } from 'chai';
-import {createExpense, updateExpense, deleteExpense, getExpenses} from '../../controllers/expenseController.js';
+import {createExpense, updateExpense, deleteExpense, getExpenses, getExpenseSummary} from '../../controllers/expenseController.js';
 import Expense from '../../models/expense';
 import ExpenseCategory from '../../models/expenseCategory';
 import '../test_helper.js';
+
+// Express test server
+const app = express();
+app.use(express.json()); 
+
+// Routes for expense controller tests
+app.post('/expenses', createExpense);
+app.put('/expenses/:id', updateExpense);
+app.delete('/expenses/:id',deleteExpense);
+app.get('/expenses', getExpenses);
+app.get('/expense-summary', getExpenseSummary);
+
+// Test suite for expense controller
+describe('Expense Controller', () => {
+    
+});
