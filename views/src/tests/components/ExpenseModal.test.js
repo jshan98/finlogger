5602ed17c569ce35c0436b98668588054321c64e
jshan98 <jshanahan@ggc.edu>
2026-05-14 15:27:54 -0400

    Made testing enviroment, config files for babel and jest, deleted App.test.js, made styleMock to mock bootstrap for testing, made test suite for ExpenseModal

--- views/src/tests/components/ExpenseModal.test.js
+++ views/src/tests/components/ExpenseModal.test.js
@@ -3,8 +3,8 @@ import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import ExpenseModal from "../../components/ExpenseModal";
 import { useExpenseModal } from "../../context/ExpenseModalContext";
 import { useAppContext } from "../../context/AppContext";
-import '@testing-library/jest-dom/extend-expect';
-import { expect } from "chai";
+import '@testing-library/jest-dom';
+//import { expect } from "chai";
 
 // Mocks ExpenseModalContext - useExpenseModal hook
 jest.mock('../../context/ExpenseModalContext', () => ({
@@ -51,54 +51,54 @@ describe('Expense Modal Component', () => {
                 json: () => Promise.resolve({}),
             })
         );
+    });
+    
+    // Test 1: Show validation errors if form is submitted with empty fields
+    test('Show validation errors if form is submitted with empty fields', async () => {
+        render(<ExpenseModal />); // Renders ExpenseModal component
 
-        // Test 1: Show validation errors if form is submitted with empty fields
-        test('', async () => {
-            render(<ExpenseModal />); // Renders ExpenseModal component
+        fireEvent.click(screen.getByText('Submit')); // Simulates clicking the submit button
 
-            fireEvent.click(screen.getByText('Submit')); // Simulates clicking the submit button
-
-            // Waits for validation messages to appear, then verifies them.
-            await waitFor(() => {
-                expect(screen.getByText('Please provide a valid date.')).toBeInTheDocument();
-                expect(screen.getByText('Please select a category.')).toBeInTheDocument();
-                expect(screen.getByText('Please provide a description.')).toBeInTheDocument();
-                expect(screen.getByText('Please provide an amount.')).toBeInTheDocument();
-            });
+        // Waits for validation messages to appear, then verifies them.
+        await waitFor(() => {
+            expect(screen.getByText('Please provide a valid date.')).toBeInTheDocument();
+            expect(screen.getByText('Please select a category.')).toBeInTheDocument();
+            expect(screen.getByText('Please provide a description.')).toBeInTheDocument();
+            expect(screen.getByText('Please provide an amount.')).toBeInTheDocument();
         });
+    });
 
-        // Test 2: Simulates form submission and verifies the API call
-        test('', async () => {
-            render(<ExpenseModal />); // Renders ExpenseModal component
-            // Fills date with field '2021-07-20'
-            fireEvent.change(screen.getByLabelText('Date'), {target : {value : '2021-07-20'}});
-            // Fills category with field 'Groceries'
-            fireEvent.change(screen.getByLabelText('Category'), {target : {value : 'Groceries'}});
-            // Fills description with field 'Milk and Eggs'
-            fireEvent.change(screen.getByLabelText('Description'), {target : {value : 'Milk and Eggs'}});
-            // Fills amount with field '10'
-            fireEvent.change(screen.getByLabelText('Amount'), {target : {value : '10'}});
-            // Simulates the clicking of the submit button
-            fireEvent.click(screen.getByText('Submit'));
-            
-            // Asserts - Waits for the fetch API call and verifies that it was made with the correct parameters
-            await waitFor(() => {
-                expect(fetch).toHaveBeenCalledWith('http://localhost:3001/expenses', {
-                    method: 'POST',
-                    headers: {'content-type' : 'application/json'},
-                    body: JSON.stringify({
-                        user_id: 'USER_1',
-                        description: 'Milk and Eggs',
-                        amount: 10,
-                        date: '2021-07-20',
-                        categoryName: 'Groceries'
-                    })
-                });
-                // Verifies that the showToast and data refresh were triggered
-                expect(showToastMock).toHaveBeenCalledWith('Expense submitted successfully!');
-                expect(fetchExpenseDataMock).toHaveBeenCalled();
-                expect(handleCloseMock).toHaveBeenCalled(); // Ensures that the modal has been closed after submission
+    // Test 2: Simulates form submission and verifies the API call
+    test('Simulates form submission and verifies the API call', async () => {
+        render(<ExpenseModal />); // Renders ExpenseModal component
+        // Fills date with field '2021-07-20'
+        fireEvent.change(screen.getByLabelText('Date'), {target : {value : '2021-07-20'}});
+        // Fills category with field 'Groceries'
+        fireEvent.change(screen.getByLabelText('Category'), {target : {value : 'Groceries'}});
+        // Fills description with field 'Milk and Eggs'
+        fireEvent.change(screen.getByLabelText('Description'), {target : {value : 'Milk and Eggs'}});
+        // Fills amount with field '10'
+        fireEvent.change(screen.getByLabelText('Amount'), {target : {value : '10'}});
+        // Simulates the clicking of the submit button
+        fireEvent.click(screen.getByText('Submit'));
+        
+        // Asserts - Waits for the fetch API call and verifies that it was made with the correct parameters
+        await waitFor(() => {
+            expect(fetch).toHaveBeenCalledWith('http://localhost:3001/expenses/', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify({
+                    user_id: 'USER_1',
+                    description: 'Milk and Eggs',
+                    amount: 10,
+                    date: '2021-07-20',
+                    categoryName: 'Groceries'
+                })
             });
+            // Verifies that the showToast and data refresh were triggered
+            expect(showToastMock).toHaveBeenCalledWith('Expense submitted successfully!');
+            expect(fetchExpenseDataMock).toHaveBeenCalled();
+            expect(handleCloseMock).toHaveBeenCalled(); // Ensures that the modal has been closed after submission
         });
     });
 });
